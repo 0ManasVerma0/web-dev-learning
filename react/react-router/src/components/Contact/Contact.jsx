48b9d41e35772b1a031d5df6dd2e25f5076ffa80
Manas verma <manasverma1357@gmail.com>
2025-05-25 00:19:31 +0530

    react-router

---
+++ react/react-router/src/components/Contact/Contact.jsx
@@ -0,0 +1,8 @@
+import React from 'react'
+import {Link} from 'react-router-dom'
+
+export default function Contact(){
+    return (
+        <h1 className='bg-gray-500 text-5xl'>You can contact me at -- manasverma1357@gmail.com</h1>
+    )
+}
